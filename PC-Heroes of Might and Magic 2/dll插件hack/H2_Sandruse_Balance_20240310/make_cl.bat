67d920b2a4c5f72d0f42f2a632a3591766127ac8
AK6626 <74704024+AK6626@users.noreply.github.com>
2026-04-20 21:33:41 +0800

    Delete PC-Heroes of Might and Magic 2/dll插件hack/H2_Sandruse_Balance_20240310 directory

--- PC-Heroes of Might and Magic 2/dll插件hack/H2_Sandruse_Balance_20240310/make_cl.bat
+++
@@ -1,2 +0,0 @@
-cl 3.cpp /LD /Zi /nologo /W3 /WX- /O2 /Oi /Oy- /GL /D WIN32 /D NDEBUG /D _WINDOWS /D _USRDLL /D MY222_EXPORTS /D _WINDLL /D _UNICODE /D UNICODE /Gm- /EHsc /MD /GS /Gy /fp:precise /Zc:wchar_t /Zc:forScope /Fo"Release\\" /Fd"Release\vc100.pdb" /Gd /TP /analyze- /errorReport:prompt
-pause
